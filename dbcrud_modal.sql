2bf9fc3beca14b10c97a80f831a1e62b48e31c2e
RezaPahlevii <rezapahlevih2o@gmail.com>
2022-08-26 15:12:54 +0700

    + db

---
+++ dbcrud_modal.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Agu 2022 pada 10.09
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbcrud_modal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tmhs`
+--
+
+CREATE TABLE `tmhs` (
+  `id_mhs` int(11) NOT NULL,
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `prodi` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tmhs`
+--
+ALTER TABLE `tmhs`
+  ADD PRIMARY KEY (`id_mhs`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tmhs`
+--
+ALTER TABLE `tmhs`
+  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
